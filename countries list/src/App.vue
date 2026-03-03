541f3447dde9f79779d6b94dc03d59901d2baede
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2026-03-03 16:53:06 +0500

    Создание компонент Поиск и список стран
    Подключил vue.js,  создал две компоненты поиск и список стран
    использовал директивы v-model, v-for

---
+++ countries list/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import SearchComponent from "@/components/searchComponent.vue";
+import CountriesComponent from "@/components/countriesComponent.vue";
+
+const countries = ref([
+      {flag: '/flags/azerbaijan.png', name: 'Азербайджан', price: '99',},
+      {flag: '/flags/armenia.png', name: 'Армения', price: '99',},
+      {flag: '/flags/germany.png', name: 'Германия', price: '99',},
+      {flag: '/flags/egypt.png', name: 'Египет ', price: '99',},
+      {flag: '/flags/italy.png', name: 'Италия', price: '99',},
+      {flag: '/flags/china.png', name: 'Китай', price: '99',},
+      {flag: '/flags/emirates.png', name: 'ОАЭ', price: '99',},
+      {flag: '/flags/france.png', name: 'Франция', price: '99',},
+    ]
+)
+</script>
+
+<template>
+<div class="wrapper">
+  <p class="title">Ближние дали</p>
+  <search-component />
+  <countries-component :countries='countries'/>
+</div>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 400px;
+  margin: 0 auto;
+  font-family: Arial;
+  display: flex;
+  flex-direction: column;
+
+}
+
+.title{
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+}
+</style>
---
+++ countries list/src/components/countriesComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+let props = defineProps({
+  countries: Array
+})
+</script>
+
+<template>
+  <div class="countries_container">
+    <ul class="countries_list">
+      <li v-for="country in props.countries"
+          class="country_element">
+        <img :src="country.flag" :alt="country.name"  class="country_flag" />
+        <p class="country_name">{{country.name}}</p>
+        <p class="country_price">{{country.price}} ₸/мин</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.country_element{
+  list-style: none;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 600;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
